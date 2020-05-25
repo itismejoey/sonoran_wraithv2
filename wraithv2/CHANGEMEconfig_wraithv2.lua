ba231e08d3b6b672dd00756d0b17dd31c441aec1
Nathan C <39100023+nathanctech@users.noreply.github.com>
2020-05-24 23:25:15 -0400

    bugfixes: Multiple bugfixes from user reports (#4)
    * bugfixes: Multiple bugfixes from user reports
    Add optional checks for expiration
    Add optional checks for middle initial
    Add config options for both checks
    Don't show notifications on VALID plates (antispam)
    Fix null check
    Fixes #1, #2, #3
    
    
    * MAJOR: Update to use new config methods
    
    * fix: plate lookups not working sometimes

--- wraithv2/CHANGEMEconfig_wraithv2.lua
+++ wraithv2/CHANGEMEconfig_wraithv2.lua
@@ -6,13 +6,17 @@
     Put all needed configuration in this file.
 ]]
 local config = {
+    enabled = false,
     pluginName = "wraithv2", -- name your plugin here
-    pluginVersion = "1.0", -- version of your plugin
     pluginAuthor = "SonoranCAD", -- author
     requiresPlugins = {"lookups"}, -- required plugins for this plugin to work, separated by commas
 
     -- put your configuration options below
     isPluginEnabled = false -- disable this plugin by default
+    ,useExpires = true -- use vehicle registration expirations, or not
+    ,useMiddleInitial = true -- use middle initials?
 }
 
---Config.RegisterPluginConfig(config.pluginName, config)
+if config.enabled then
+    Config.RegisterPluginConfig(config.pluginName, config)
+end
--- wraithv2/sv_wraithv2.lua
+++ wraithv2/sv_wraithv2.lua
@@ -8,83 +8,69 @@
     Put all server-side logic in this file.
 ]]
 
-local pluginConfig = Config.plugins["wraithv2"]
+local pluginConfig = Config.GetPluginConfig("wraithv2")
 
-wraithLastPlates = { locked = nil, scanned = nil }
+if pluginConfig.enabled then
 
-exports('cadGetLastPlates', function() return wraithLastPlates end)
+    if pluginConfig.useExpires == nil then
+        pluginConfig.useExpires = true
+    end
+    if pluginConfig.useMiddleInitial == nil then
+        pluginConfig.useMiddleInitial = true
+    end
 
-CreateThread(function()
-    if pluginConfig.isPluginEnabled then
-        RegisterNetEvent("wk:onPlateLocked")
-        AddEventHandler("wk:onPlateLocked", function(cam, plate, index)
-            debugLog(("plate lock: %s - %s - %s"):format(cam, plate, index))
-            local source = source
-            local ids = GetIdentifiers(source)
-            wraithLastPlates.locked = { cam = cam, plate = plate, index = index, vehicle = cam.vehicle }
-            cadPlateLookup(plate, false, function(data)
-                if data == nil then
-                    debugLog("No data returned")
-                    return
-                end
-                local reg = data.vehicleRegistrations[1] -- scanner is always full lookup
-                local bolos = data.bolos
-                if reg then
-                    TriggerEvent("SonoranCAD::wraithv2:PlateLocked", source, reg, cam, plate, index)
-                    local mi = reg.person.mi ~= "" and ", "..reg.person.mi or ""
-                    debugLog(("DATA: Plate [%s]: S: %s E: %s O: %s"):format(reg.vehicle.plate, reg.status, reg.expiration, reg.person.first.." "..reg.person.last..mi))
-                    
-                    TriggerClientEvent("pNotify:SendNotification", source, {
-                        text = "<b style='color:yellow'>"..cam.." ALPR</b><br/>Plate: "..reg.vehicle.plate.."<br/>Status: "..reg.status.."<br/>Expires: "..reg.expiration.."<br/>Owner: "..reg.person.first.." "..reg.person.last..mi,
-                        type = "success",
-                        queue = "alpr",
-                        timeout = 45000,
-                        layout = "centerLeft"
-                    })
-                    if bolos then
-                        if bolos[1].flags then
-                            local flags = table.concat(bolos[1].flags, ",")
-                            TriggerClientEvent("pNotify:SendNotification", source, {
-                                text = ("<b style='color:red'>BOLO ALERT!<br/>Plate: %s<br/>Flags: %s"):format(reg.vehicle.plate, flags),
-                                type = "error",
-                                queue = "bolo",
-                                timeout = 20000,
-                                layout = "centerLeft"
-                            })
+    wraithLastPlates = { locked = nil, scanned = nil }
+
+    exports('cadGetLastPlates', function() return wraithLastPlates end)
+
+    CreateThread(function()
+        if pluginConfig.isPluginEnabled then
+            RegisterNetEvent("wk:onPlateLocked")
+            AddEventHandler("wk:onPlateLocked", function(cam, plate, index)
+                debugLog(("plate lock: %s - %s - %s"):format(cam, plate, index))
+                local source = source
+                local ids = GetIdentifiers(source)
+                plate = plate:gsub("%s+", "")
+                wraithLastPlates.locked = { cam = cam, plate = plate, index = index, vehicle = cam.vehicle }
+                cadPlateLookup(plate, false, function(data)
+                    if data == nil or data.vehicleRegistrations == nil then
+                        debugLog("No data returned")
+                        return
+                    end
+                    local reg = false
+                    for _, veh in pairs(data.vehicleRegistrations) do
+                        if veh.vehicle.plate == plate then
+                            reg = veh
+                            break
                         end
                     end
-                else
-                    TriggerClientEvent("pNotify:SendNotification", source, {
-                        text = "<b style='color:yellow'>"..cam.." ALPR</b><br/>Plate: "..plate.."<br/>Status: Not Registered",
-                        type = "error",
-                        queue = "alpr",
-                        timeout = 15000,
-                        layout = "centerLeft"
-                    })
-                end
-            end, ids[Config.primaryIdentifier])
-        end)
-
-        RegisterNetEvent("wk:onPlateScanned")
-        AddEventHandler("wk:onPlateScanned", function(cam, plate, index)
-            debugLog(("plate scan: %s - %s - %s"):format(cam, plate, index))
-            local source = source
-            wraithLastPlates.scanned = { cam = cam, plate = plate, index = index, vehicle = cam.vehicle }
-            TriggerEvent("SonoranCAD::wraithv2:PlateScanned", source, reg, cam, plate, index)
-            cadPlateLookup(plate, true, function(data)
-                if data ~= nil then
-                    local reg = data.vehicleRegistrations[1] -- scanner is always full lookup
+                    local bolos = #data.bolos and data.bolos or false
                     if reg then
-                        local mi = reg.person.mi ~= "" and ", "..reg.person.mi or ""
-                        debugLog(("DATA: Plate [%s]: S: %s E: %s O: %s"):format(reg.vehicle.plate, reg.status, reg.expiration, reg.person.first.." "..reg.person.last..mi))
+                        TriggerEvent("SonoranCAD::wraithv2:PlateLocked", source, reg, cam, plate, index)
+                        local plate = reg.vehicle.plate
+                        local status = reg.status
+                        local expires = (reg.expiration and pluginConfig.useExpires) and ("Expires: %s<br/>"):format(reg.expiration) or ""
+                        local owner = pluginConfig.useMiddleInitial and ("%s %s, %s"):format(reg.person.first, reg.person.last, reg.person.mi) or ("%s %s"):format(reg.person.first, reg.person.last)
                         
                         TriggerClientEvent("pNotify:SendNotification", source, {
-                            text = "<b style='color:yellow'>"..cam.." ALPR</b><br/>Plate: "..reg.vehicle.plate.."<br/>Status: "..reg.status.."<br/>Expires: "..reg.expiration.."<br/>Owner: "..reg.person.first.." "..reg.person.last..mi,
+                            text = ("<b style='color:yellow'>"..cam.." ALPR</b><br/>Plate: %s<br/>Status: %s<br/>%sOwner: %s"):format(plate, status, expires, owner),
                             type = "success",
                             queue = "alpr",
-                            timeout = 45000,
+                            timeout = 30000,
                             layout = "centerLeft"
                         })
+                        if #bolos > 0 then
+                            if bolos[1].flags then
+                                local flags = table.concat(bolos[1].flags, ",")
+                                TriggerClientEvent("pNotify:SendNotification", source, {
+                                    text = ("<b style='color:red'>BOLO ALERT!<br/>Plate: %s<br/>Flags: %s"):format(reg.vehicle.plate, flags),
+                                    type = "error",
+                                    queue = "bolo",
+                                    timeout = 20000,
+                                    layout = "centerLeft"
+                                })
+                            end
+                        end
                     else
                         TriggerClientEvent("pNotify:SendNotification", source, {
                             text = "<b style='color:yellow'>"..cam.." ALPR</b><br/>Plate: "..plate.."<br/>Status: Not Registered",
@@ -94,9 +80,53 @@ CreateThread(function()
                             layout = "centerLeft"
                         })
                     end
-                end
+                end, ids[Config.primaryIdentifier])
             end)
-        end)
-    end
-end)
 
+            RegisterNetEvent("wk:onPlateScanned")
+            AddEventHandler("wk:onPlateScanned", function(cam, plate, index)
+                debugLog(("plate scan: %s - %s - %s"):format(cam, plate, index))
+                local source = source
+                plate = plate:gsub("%s+", "")
+                wraithLastPlates.scanned = { cam = cam, plate = plate, index = index, vehicle = cam.vehicle }
+                TriggerEvent("SonoranCAD::wraithv2:PlateScanned", source, reg, cam, plate, index)
+                cadPlateLookup(plate, true, function(data)
+                    if data ~= nil and data.vehicleRegistrations ~= nil then
+                        local reg = false
+                        for _, veh in pairs(data.vehicleRegistrations) do
+                            if veh.vehicle.plate == plate then
+                                reg = veh
+                                break
+                            end
+                        end
+                        if reg then
+                            local mi = reg.person.mi ~= "" and ", "..reg.person.mi or ""
+                            local plate = reg.vehicle.plate
+                            local status = reg.status
+                            local expires = (reg.expiration and pluginConfig.useExpires) and ("Expires: %s<br/>"):format(reg.expiration) or ""
+                            local owner = pluginConfig.useMiddleInitial and ("%s %s, %s"):format(reg.person.first, reg.person.last, reg.person.mi) or ("%s %s"):format(reg.person.first, reg.person.last)
+                            if status ~= "VALID" then
+                                TriggerClientEvent("pNotify:SendNotification", source, {
+                                    text = ("<b style='color:yellow'>"..cam.." ALPR</b><br/>Plate: %s<br/>Status: %s<br/>%sOwner: %s"):format(plate, status, expires, owner),
+                                    type = "success",
+                                    queue = "alpr",
+                                    timeout = 30000,
+                                    layout = "centerLeft"
+                                })
+                            end
+                        else
+                            TriggerClientEvent("pNotify:SendNotification", source, {
+                                text = "<b style='color:yellow'>"..cam.." ALPR</b><br/>Plate: "..plate.."<br/>Status: Not Registered",
+                                type = "error",
+                                queue = "alpr",
+                                timeout = 15000,
+                                layout = "centerLeft"
+                            })
+                        end
+                    end
+                end)
+            end)
+        end
+    end)
+
+end
