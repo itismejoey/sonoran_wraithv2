30171218a80ff2d6b0edc259efd41c33f9f7bc3a
Nathan C <me@nathanc.tech>
2020-09-11 02:12:11 -0400

    fix: remove duplicate enabled config option

--- wraithv2/CHANGEMEconfig_wraithv2.lua
+++ wraithv2/CHANGEMEconfig_wraithv2.lua
@@ -12,7 +12,6 @@ local config = {
     requiresPlugins = {"lookups"}, -- required plugins for this plugin to work, separated by commas
 
     -- put your configuration options below
-    isPluginEnabled = false -- disable this plugin by default
     ,useExpires = true -- use vehicle registration expirations, or not
     ,useMiddleInitial = true -- use middle initials?
     ,alertNoRegistration = true -- alert if no registration was found on scan?
--- wraithv2/sv_wraithv2.lua
+++ wraithv2/sv_wraithv2.lua
@@ -23,143 +23,139 @@ if pluginConfig.enabled then
 
     exports('cadGetLastPlates', function() return wraithLastPlates end)
 
-    CreateThread(function()
-        if pluginConfig.isPluginEnabled then
-            RegisterNetEvent("wk:onPlateLocked")
-            AddEventHandler("wk:onPlateLocked", function(cam, plate, index)
-                debugLog(("plate lock: %s - %s - %s"):format(cam, plate, index))
-                local source = source
-                local ids = GetIdentifiers(source)
-                plate = plate:match("^%s*(.-)%s*$")
-                wraithLastPlates.locked = { cam = cam, plate = plate, index = index, vehicle = cam.vehicle }
-                cadGetInformation(plate, function(regData, vehData, charData, boloData)
-                    if cam == "front" then
-                        camCapitalized = "Front"
-                    elseif cam == "rear" then
-                        camCapitalized = "Rear"
-                    end
-                    if #vehData < 1 then
-                        debugLog("No data returned")
-                        return
-                    end
-                    local reg = false
-                    for _, veh in pairs(vehData) do
-                        if veh.plate:lower() == plate:lower() then
-                            reg = veh
-                            break
-                        end
-                    end
-                    if #charData < 1 then
-                        debugLog("Invalid registration")
-                        return
-                    end
-                    local person = charData[1]
-                    if reg then
-                        TriggerEvent("SonoranCAD::wraithv2:PlateLocked", source, reg, cam, plate, index)
-                        local plate = reg.plate
-                        local status = regData[1]["Status"]
-                        local expires = (regData[1]["Expiration"] and pluginConfig.useExpires) and ("Expires: %s<br/>"):format(regData[1]["Expiration"]) or ""
-                        local owner = pluginConfig.useMiddleInitial and ("%s %s, %s"):format(person.first, person.last, person.mi) or ("%s %s"):format(person.first, person.last)
-                        TriggerClientEvent("pNotify:SendNotification", source, {
-                            text = ("<b style='color:yellow'>"..camCapitalized.." ALPR</b><br/>Plate: %s<br/>Status: %s<br/>%sOwner: %s"):format(plate:upper(), status, expires, owner),
-                            type = "success",
-                            queue = "alpr",
-                            timeout = 30000,
-                            layout = "centerLeft"
-                        })
-                        if #boloData > 0 then
-                            local flags = table.concat(boloData, ",")
-                            TriggerClientEvent("pNotify:SendNotification", source, {
-                                text = ("<b style='color:red'>BOLO ALERT!<br/>Plate: %s<br/>Flags: %s"):format(plate:upper(), flags),
-                                type = "error",
-                                queue = "bolo",
-                                timeout = 20000,
-                                layout = "centerLeft"
-                            })
-                        end
-                    else
-                        TriggerClientEvent("pNotify:SendNotification", source, {
-                            text = "<b style='color:yellow'>"..camCapitalized.." ALPR</b><br/>Plate: "..plate:upper().."<br/>Status: Not Registered",
-                            type = "error",
-                            queue = "alpr",
-                            timeout = 15000,
-                            layout = "centerLeft"
-                        })
-                    end
-                end, ids[Config.primaryIdentifier])
-            end)
-
-            RegisterNetEvent("wk:onPlateScanned")
-            AddEventHandler("wk:onPlateScanned", function(cam, plate, index)
-                if cam == "front" then
-                    camCapitalized = "Front"
-                elseif cam == "rear" then
-                    camCapitalized = "Rear"
+    RegisterNetEvent("wk:onPlateLocked")
+    AddEventHandler("wk:onPlateLocked", function(cam, plate, index)
+        debugLog(("plate lock: %s - %s - %s"):format(cam, plate, index))
+        local source = source
+        local ids = GetIdentifiers(source)
+        plate = plate:match("^%s*(.-)%s*$")
+        wraithLastPlates.locked = { cam = cam, plate = plate, index = index, vehicle = cam.vehicle }
+        cadGetInformation(plate, function(regData, vehData, charData, boloData)
+            if cam == "front" then
+                camCapitalized = "Front"
+            elseif cam == "rear" then
+                camCapitalized = "Rear"
+            end
+            if #vehData < 1 then
+                debugLog("No data returned")
+                return
+            end
+            local reg = false
+            for _, veh in pairs(vehData) do
+                if veh.plate:lower() == plate:lower() then
+                    reg = veh
+                    break
                 end
-                debugLog(("plate scan: %s - %s - %s"):format(cam, plate, index))
-                local source = source
-                plate = plate:match("^%s*(.-)%s*$")
-                wraithLastPlates.scanned = { cam = cam, plate = plate, index = index, vehicle = cam.vehicle }
-                TriggerEvent("SonoranCAD::wraithv2:PlateScanned", source, reg, cam, plate, index)
-                cadGetInformation(plate, function(regData, vehData, charData, boloData)
-                    if cam == "front" then
-                        camCapitalized = "Front"
-                    elseif cam == "rear" then
-                        camCapitalized = "Rear"
-                    end
-                    local reg = false
-                    for _, veh in pairs(vehData) do
-                        if veh.plate:lower() == plate:lower() then
-                            reg = veh
-                            break
-                        end
-                    end
-                    local person = {}
-                    if #charData > 0 then
-                        person = charData[1]
-                    end
-                    if reg then
-                        TriggerEvent("SonoranCAD::wraithv2:PlateLocked", source, reg, cam, plate, index)
-                        local plate = reg.plate
-                        local status = regData[1]["Status"]
-                        local expires = (regData[1]["Expiration"] and pluginConfig.useExpires) and ("Expires: %s<br/>"):format(regData[1]["Expiration"]) or ""
-                        local owner = pluginConfig.useMiddleInitial and ("%s %s, %s"):format(person.first, person.last, person.mi) or ("%s %s"):format(person.first, person.last)
-                        if status ~= "VALID" then
-                            TriggerClientEvent("pNotify:SendNotification", source, {
-                                text = ("<b style='color:yellow'>"..camCapitalized.." ALPR</b><br/>Plate: %s<br/>Status: %s<br/>%sOwner: %s"):format(plate:upper(), status, expires, owner),
-                                type = "success",
-                                queue = "alpr",
-                                timeout = 10000,
-                                layout = "centerLeft"
-                            })
-                            TriggerEvent("SonoranCAD::wraithv2:BadStatus", plate, status, regData[1]["Expiration"], owner)
-                        end
-                        if #boloData > 0 then
-                            local flags = table.concat(boloData, ",")
-                            TriggerClientEvent("pNotify:SendNotification", source, {
-                                text = ("<b style='color:red'>BOLO ALERT!<br/>Plate: %s<br/>Flags: %s"):format(plate:upper(), flags),
-                                type = "error",
-                                queue = "bolo",
-                                timeout = 20000,
-                                layout = "centerLeft"
-                            })
-                            TriggerEvent("SonoranCAD::wraithv2:BoloAlert", plate, flags)
-                        end
-                    else
-                        if pluginConfig.alertNoRegistration then
-                            TriggerClientEvent("pNotify:SendNotification", source, {
-                                text = "<b style='color:yellow'>"..camCapitalized.." ALPR</b><br/>Plate: "..plate:upper().."<br/>Status: Not Registered",
-                                type = "warning",
-                                queue = "alpr",
-                                timeout = 5000,
-                                layout = "centerLeft"
-                            })
-                            TriggerEvent("SonoranCAD::wraithv2:NoRegAlert", plate)
-                        end
-                    end
-                end)
-            end)
+            end
+            if #charData < 1 then
+                debugLog("Invalid registration")
+                return
+            end
+            local person = charData[1]
+            if reg then
+                TriggerEvent("SonoranCAD::wraithv2:PlateLocked", source, reg, cam, plate, index)
+                local plate = reg.plate
+                local status = regData[1]["Status"]
+                local expires = (regData[1]["Expiration"] and pluginConfig.useExpires) and ("Expires: %s<br/>"):format(regData[1]["Expiration"]) or ""
+                local owner = pluginConfig.useMiddleInitial and ("%s %s, %s"):format(person.first, person.last, person.mi) or ("%s %s"):format(person.first, person.last)
+                TriggerClientEvent("pNotify:SendNotification", source, {
+                    text = ("<b style='color:yellow'>"..camCapitalized.." ALPR</b><br/>Plate: %s<br/>Status: %s<br/>%sOwner: %s"):format(plate:upper(), status, expires, owner),
+                    type = "success",
+                    queue = "alpr",
+                    timeout = 30000,
+                    layout = "centerLeft"
+                })
+                if #boloData > 0 then
+                    local flags = table.concat(boloData, ",")
+                    TriggerClientEvent("pNotify:SendNotification", source, {
+                        text = ("<b style='color:red'>BOLO ALERT!<br/>Plate: %s<br/>Flags: %s"):format(plate:upper(), flags),
+                        type = "error",
+                        queue = "bolo",
+                        timeout = 20000,
+                        layout = "centerLeft"
+                    })
+                end
+            else
+                TriggerClientEvent("pNotify:SendNotification", source, {
+                    text = "<b style='color:yellow'>"..camCapitalized.." ALPR</b><br/>Plate: "..plate:upper().."<br/>Status: Not Registered",
+                    type = "error",
+                    queue = "alpr",
+                    timeout = 15000,
+                    layout = "centerLeft"
+                })
+            end
+        end, ids[Config.primaryIdentifier])
+    end)
+
+    RegisterNetEvent("wk:onPlateScanned")
+    AddEventHandler("wk:onPlateScanned", function(cam, plate, index)
+        if cam == "front" then
+            camCapitalized = "Front"
+        elseif cam == "rear" then
+            camCapitalized = "Rear"
         end
+        debugLog(("plate scan: %s - %s - %s"):format(cam, plate, index))
+        local source = source
+        plate = plate:match("^%s*(.-)%s*$")
+        wraithLastPlates.scanned = { cam = cam, plate = plate, index = index, vehicle = cam.vehicle }
+        TriggerEvent("SonoranCAD::wraithv2:PlateScanned", source, reg, cam, plate, index)
+        cadGetInformation(plate, function(regData, vehData, charData, boloData)
+            if cam == "front" then
+                camCapitalized = "Front"
+            elseif cam == "rear" then
+                camCapitalized = "Rear"
+            end
+            local reg = false
+            for _, veh in pairs(vehData) do
+                if veh.plate:lower() == plate:lower() then
+                    reg = veh
+                    break
+                end
+            end
+            local person = {}
+            if #charData > 0 then
+                person = charData[1]
+            end
+            if reg then
+                TriggerEvent("SonoranCAD::wraithv2:PlateLocked", source, reg, cam, plate, index)
+                local plate = reg.plate
+                local status = regData[1]["Status"]
+                local expires = (regData[1]["Expiration"] and pluginConfig.useExpires) and ("Expires: %s<br/>"):format(regData[1]["Expiration"]) or ""
+                local owner = pluginConfig.useMiddleInitial and ("%s %s, %s"):format(person.first, person.last, person.mi) or ("%s %s"):format(person.first, person.last)
+                if status ~= "VALID" then
+                    TriggerClientEvent("pNotify:SendNotification", source, {
+                        text = ("<b style='color:yellow'>"..camCapitalized.." ALPR</b><br/>Plate: %s<br/>Status: %s<br/>%sOwner: %s"):format(plate:upper(), status, expires, owner),
+                        type = "success",
+                        queue = "alpr",
+                        timeout = 10000,
+                        layout = "centerLeft"
+                    })
+                    TriggerEvent("SonoranCAD::wraithv2:BadStatus", plate, status, regData[1]["Expiration"], owner)
+                end
+                if #boloData > 0 then
+                    local flags = table.concat(boloData, ",")
+                    TriggerClientEvent("pNotify:SendNotification", source, {
+                        text = ("<b style='color:red'>BOLO ALERT!<br/>Plate: %s<br/>Flags: %s"):format(plate:upper(), flags),
+                        type = "error",
+                        queue = "bolo",
+                        timeout = 20000,
+                        layout = "centerLeft"
+                    })
+                    TriggerEvent("SonoranCAD::wraithv2:BoloAlert", plate, flags)
+                end
+            else
+                if pluginConfig.alertNoRegistration then
+                    TriggerClientEvent("pNotify:SendNotification", source, {
+                        text = "<b style='color:yellow'>"..camCapitalized.." ALPR</b><br/>Plate: "..plate:upper().."<br/>Status: Not Registered",
+                        type = "warning",
+                        queue = "alpr",
+                        timeout = 5000,
+                        layout = "centerLeft"
+                    })
+                    TriggerEvent("SonoranCAD::wraithv2:NoRegAlert", plate)
+                end
+            end
+        end)
     end)
 
 end
